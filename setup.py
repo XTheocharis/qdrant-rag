e200078e1ff79c3e0bea6c7627a53b839f720cca
XTheocharis <162344168+XTheocharis@users.noreply.github.com>
2025-08-27 22:33:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="qdrant-rag",
+    name="qdrant_rag",
     version="1.0.0",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'pipeline=qdrant-rag:main_cli',
+            'pipeline=qdrant_rag:main_cli',
         ],
     },
 )
